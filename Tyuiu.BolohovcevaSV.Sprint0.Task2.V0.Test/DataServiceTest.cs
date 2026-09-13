c668e75701508dde83477856b2976fe30f42a3f3
София <hj2mctkp4w@privaterelay.appleid.com>
2026-09-14 01:32:58 +0500

    Task2

---
+++ Tyuiu.BolohovcevaSV.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.BolohovcevaSV.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.BolohovcevaSV.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "София";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, София", res);
+        }
+    }
+}
---
+++ Tyuiu.BolohovcevaSV.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.BolohovcevaSV.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.BolohovcevaSV.Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("София"));
+            Console.ReadKey();
+        }
+    }
+}
