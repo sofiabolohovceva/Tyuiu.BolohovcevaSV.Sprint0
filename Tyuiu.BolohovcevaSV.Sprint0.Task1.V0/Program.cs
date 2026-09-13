0cb384818e4aa93b4646c1888fa1ae614153a64b
София <hj2mctkp4w@privaterelay.appleid.com>
2026-09-13 18:01:41 +0500

    Task 1

---
+++ Tyuiu.BolohovcevaSV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Tyuiu.BolohovcevaSV.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вывод на консоль строку Hello, World!
+            Console.WriteLine("Hello, World!");
+
+            //Объявляем переменные
+            string name = "Игорь";
+            int age = 45;
+
+            //Вывод на консоль строку
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет.");
+
+            //Ожидание нажатия клавиши, нужно чтобы окно консоли не закрывалось
+            Console.ReadKey();
+        }
+    }
+}
+
+
