3d74a234cac450f93957359878a206e8aafeec68
София <hj2mctkp4w@privaterelay.appleid.com>
2026-09-14 14:45:37 +0500

    Task6

---
+++ Tyuiu.BolohovcevaSV.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.BolohovcevaSV.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.BolohovcevaSV.Sprint0.Task6.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arraynums = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(arraynums));
+            Console.WriteLine("Разность элементов массива = " + DataService.SubtractionArray(arraynums));
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(arraynums));
+            Console.ReadKey();
+        }
+    }
+}
